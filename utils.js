d520ee8abf8345a7a3d0f0ce6f2963eac89911ff
gaelweb <gael.pro.dev@gmail.com>
2021-06-03 09:15:35 +0200

    add utils.js and functions

---
+++ utils.js
@@ -0,0 +1,42 @@
+// Génére un nbre aléatoire arrondi.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function resultCombination(setArr) {
+    let resultCombination = [
+        {
+            value: 1,
+            result: "yams"
+        },
+        {
+            value: 2,
+            result: "Carré"
+        },
+        {
+            value: 3,
+            result: "Double Carré"
+        }
+    ]
+
+    const result = resultCombination.filter((item) => item.value === setArr.size);
+    console.log('result :', result);
+    return result.length >= 0 ? result[0].result : '';
+}
+
+const play = () => {
+    let yamsCombination = []
+    let min = 1;
+    let max = 6;
+  
+    for (let i = 0; i < 5; i++) {
+      yamsCombination.push(getRandomInt(min, max))
+    }
+
+    const setYamsCombination = new Set(yamsCombination);
+    console.log(yamsCombination, setYamsCombination);
+    const result = resultCombination(setYamsCombination); 
+    return result;
+}
+
+module.exports = play;
