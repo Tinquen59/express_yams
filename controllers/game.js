f6a751ed129fc2527112823d373994f3de9931aa
gaelweb <gael.pro.dev@gmail.com>
2021-06-06 15:08:01 +0200

    fix play function add features

--- controllers/game.js
+++ controllers/game.js
@@ -1,18 +1,35 @@
 "use strict";
 
 import { allPatries, allResults } from "../models/index";
-import { canPlay } from "../utils";
+import { canPlay, play } from "../utils";
 
 export const indexController = (req, res) => {
+    const game = req.session;
+    game.party = null;
 
     allPatries().then(
         collection => {
-            console.log(collection);
             const infoGame = {
                 canPlay: canPlay(collection),
-                isStart: false
+                isStart: true // valeur à modifier (false par defaut)
             }
-            res.render("pages/index", { data : collection , infoGame, dices: [1, 2, 3, 4, 5] , title: process.env.TITLE ?? "No Title" })
+
+            game.party = play();
+
+            game.party.then((result) => {
+                
+                console.log('controller :', game.party);
+                
+                res.render("pages/index", { 
+                    data : collection,
+                    infoGame,
+                    dices: result.dices, // array in utils.js 
+                    title: process.env.TITLE ?? "No Title",
+                    combinationName: result.message.combinationName,
+                    message: result.message.message, // message in utils.js
+                    patries: result.message.namePatries
+                })
+            });
         }
     )
 };
@@ -21,7 +38,6 @@ export const resultsController = (req, res) => {
 
     allResults().then(
         collection => {
-            console.log(collection);
             res.render("pages/results", { data: collection });
         }
     )
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import { winPatries, randomPatries } from './models/index';
+
 export const canPlay = (collection) => {
     for (const element of collection) {
         if (element.number > 0) return true;
@@ -5,37 +7,80 @@ export const canPlay = (collection) => {
     if (collection[collection.length - 1] === 0) return false;
 };
 
-
 // Génére un nbre aléatoire arrondi.
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const resultCombination = setArr => {
-    let resultCombination = [
-        {
-            value: 1,
-            result: "yams"
-        },
-        {
-            value: 2,
-            result: "Carré"
-        },
-        {
-            value: 3,
-            result: "Double paires"
+const resultCombination = async setArr => {
+    let counters = [];
+    let message = {
+        combinationName: "",
+        message: "",
+        numPatriesWin: 0,
+        namePatries: []
+    };
+
+    const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+    for (let i = 1; i <= 6; i++) {
+        counters.push(countOccurrences(setArr, i));
+    }
+
+    let occurencesFilter = counters.filter(
+        item => item === 2 
+        || item === 4 
+        || item === 5
+    ); 
+
+    function occurenceCheck(occValue) {
+        if ( occValue.length === 1 ) {
+            // Si on a une occurence de 5 ou 4 (yams ou carré)
+            if (occValue[0] === 5) {
+                message.combinationName = "Yams";
+                message.message = "Bravo vous gagnez 3 pâtisseries !";
+                message.numPatriesWin = 3;
+            } else if (occValue[0] === 4) {
+                message.combinationName = "Carré";
+                message.message = "Bravo vous gagnez 2 pâtisseries !";
+                message.numPatriesWin = 2;
+            } else {
+                message.combinationName = "";
+                message.message = "Perdu !";
+            }
+        // Si on a une double paire ([2,2])
+        } else if (occValue.length === 2) {
+          message.combinationName = "Double Paire";
+          message.message = "Bravo vous gagnez 1 pâtisserie !";
+          message.numPatriesWin = 1;
+        } else {
+            message.combinationName = "";
+            message.message = "Perdu !";
         }
-    ]
+    }
 
-    let result = [];
-    result = resultCombination.filter((item) => item.value === setArr.size);
+    occurenceCheck(occurencesFilter);
+    //console.log('message.numPatriesWin :', message.numPatriesWin);
 
-    if (result.length === 0) result.push( { result: "Loose" } );
+    if (message.numPatriesWin > 0) {
 
-    console.log('result :', result);
-    return result[0].result ;
+        const returnPatries = await winPatries(message.numPatriesWin)
+            .then((res) => {
+                console.log('res winpatries :', ...res);
+                message.numPatriesWin > 0 ? message.namePatries.push(...res) : '';
+                return message;
+            })
+            .catch((error) => {
+                console.log('error return patries :', error);
+            });
+        
+        return returnPatries;
+    } else {
+        return message;
+    }
 }
 
-export const play = () => {
+export const play = async () => {
     let yamsCombination = [];
+    let resultYams = {dices: [], message: {}};
     const yamsCounter = 5;
     let min = 1;
     let max = 6;
@@ -44,8 +89,16 @@ export const play = () => {
       yamsCombination.push(getRandomInt(min, max))
     }
 
-    const setYamsCombination = new Set(yamsCombination);
-    console.log(yamsCombination, setYamsCombination);
-    const result = resultCombination(setYamsCombination); 
-    return result;
+    //console.log(await randomPatries());
+    //console.log(randomPatry);
+
+    const resultAll = await resultCombination(yamsCombination).then((res) => {
+        resultYams = {
+            dices: yamsCombination,
+            message: res
+        }
+        return resultYams;
+    });
+    
+    return resultAll;
 }
